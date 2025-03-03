5de4c00c456d53c7b3a15da1596453b0023608f7
Adam Ghounbaz <w1975658@westminster.ac.uk>
2025-03-03 15:57:03 +0000

    fix creates

--- db/create.sql
+++ db/create.sql
@@ -9,25 +9,23 @@ DROP TABLE IF EXISTS `departments`;
 
 -- CREATE TABLES
 CREATE TABLE `departments` (
-    `department_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `department_id` INT(11) NOT NULL,
     `department_name` VARCHAR(50) NOT NULL,
 
     PRIMARY KEY (`department_id`)
 );
 
 CREATE TABLE `teams` (
-    `team_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `team_id` INT(11) NOT NULL,
     `team_name` VARCHAR(50) NOT NULL,
     `department_id` INT(11) NOT NULL,
 
     PRIMARY KEY (`team_id`)
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
-ALTER TABLE `teams`
-ADD FOREIGN KEY (`department_id`) REFERENCES `departments`(`department_id`);
-
 CREATE TABLE `employees` (
-    `employee_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `employee_id` INT(11) NOT NULL,
     `firstname` VARCHAR(50) NOT NULL,
     `lastname` VARCHAR(50) NOT NULL,
     `email` VARCHAR(50) NOT NULL,
@@ -36,23 +34,17 @@ CREATE TABLE `employees` (
     `team_id` INT(11) NOT NULL,
 
     PRIMARY KEY (`employee_id`)
+    FOREIGN KEY (`team_id`) REFERENCES `teams`(`team_id`)
 );
-
-ALTER TABLE `employees`
-ADD FOREIGN KEY (`team_id`) REFERENCES `teams`(`team_id`);
-
 
 CREATE TABLE `employee_teams` (
     `employee_id` INT(11) NOT NULL,
     `team_id` INT(11) NOT NULL,
 
     PRIMARY KEY (`employee_id`, `team_id`)
+    FOREIGN KEY (`employee_id`) REFERENCES `employees`(`employee_id`)
+    FOREIGN KEY (`team_id`) REFERENCES `teams`(`team_id`)
 );
-
-ALTER TABLE `employee_teams`
-ADD FOREIGN KEY (`employee_id`) REFERENCES `employees`(`employee_id`);
-ALTER TABLE `employee_teams`
-ADD FOREIGN KEY (`team_id`) REFERENCES `teams`(`team_id`);
 
 CREATE TABLE `health_check_types` (
     `type_id` VARCHAR(50) NOT NULL,
@@ -62,16 +54,14 @@ CREATE TABLE `health_check_types` (
 );
 
 CREATE TABLE `health_checks` (
-    `check_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `check_id` INT(11) NOT NULL,
     `employee_id` INT(11) NOT NULL,
     `team_id` INT(11) NOT NULL,
     `timestamp` TIMESTAMP NOT NULL,
 
     PRIMARY KEY (`check_id`)
+    FOREIGN KEY (`employee_id`, `team_id`) REFERENCES `employee_teams`(`employee_id`, `team_id`)
 );
-
-ALTER TABLE `health_checks`
-ADD FOREIGN KEY (`employee_id`, `team_id`) REFERENCES `employee_teams`(`employee_id`, `team_id`);
 
 CREATE TABLE `health_check_votes` (
     `check_id` INT(11) NOT NULL,
@@ -80,13 +70,6 @@ CREATE TABLE `health_check_votes` (
     `direction` VARCHAR(50) NOT NULL,
 
     PRIMARY KEY (`check_id`, `type_id`)
+    FOREIGN KEY (`check_id`) REFERENCES `health_checks`(`check_id`)
+    FOREIGN KEY (`type_id`) REFERENCES `health_check_types`(`type_id`)
 );
-
-ALTER TABLE `health_check_votes`
-ADD FOREIGN KEY (`check_id`) REFERENCES `health_checks`(`check_id`);
-ALTER TABLE `health_check_votes`
-ADD FOREIGN KEY (`type_id`) REFERENCES `health_check_types`(`type_id`);
-
-
-
-
