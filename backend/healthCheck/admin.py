830eb9cf01a99774155300cac48677c516097e0b
Tobias Graski <57156093+t-graski@users.noreply.github.com>
2025-03-24 14:21:35 +0000

    Add models

--- backend/healthCheck/admin.py
+++ backend/healthCheck/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import HealthCheck, Department, Employee, EmployeeTeams, HealthCheckType, HealthCheckVotes, Team
+
 # Register your models here.
+
+admin.site.register(HealthCheck)
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(EmployeeTeams)
+admin.site.register(HealthCheckType)
+admin.site.register(HealthCheckVotes)
+admin.site.register(Team)
--- backend/healthCheck/models.py
+++ backend/healthCheck/models.py
@@ -8,7 +8,7 @@ class Employee(models.Model):
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=50)
     roleType = models.CharField(max_length=50)
-    teamId = models.ForeignKey("Team", on_delete=models.CASCADE())
+    teamId = models.ForeignKey("Team", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.firstname + " " + self.lastname
@@ -17,12 +17,12 @@ class Employee(models.Model):
 class Team(models.Model):
     teamId = models.AutoField(primary_key=True)
     teamName = models.CharField(max_length=50)
-    departmentId = models.ForeignKey("Department", on_delete=models.CASCADE())
+    departmentId = models.ForeignKey("Department", on_delete=models.CASCADE)
 
 
 class EmployeeTeams(models.Model):
-    teamId = models.ForeignKey(Team, on_delete=models.CASCADE())
-    employeeId = models.ForeignKey(Employee, on_delete=models.CASCADE())
+    teamId = models.ForeignKey(Team, on_delete=models.CASCADE)
+    employeeId = models.ForeignKey(Employee, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
@@ -43,14 +43,14 @@ class HealthCheckType(models.Model):
 class HealthCheck(models.Model):
     checkId = models.AutoField(primary_key=True)
     #     composite key from employee teams
-    teamId = models.ForeignKey(Team, on_delete=models.CASCADE())
-    employeeId = models.ForeignKey(Employee, on_delete=models.CASCADE())
+    teamId = models.ForeignKey(Team, on_delete=models.CASCADE)
+    employeeId = models.ForeignKey(Employee, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
 class HealthCheckVotes(models.Model):
-    checkId = models.ForeignKey(HealthCheck, on_delete=models.CASCADE())
-    typeId = models.ForeignKey(HealthCheckType, on_delete=models.CASCADE())
+    checkId = models.ForeignKey(HealthCheck, on_delete=models.CASCADE)
+    typeId = models.ForeignKey(HealthCheckType, on_delete=models.CASCADE)
     vote = models.CharField(max_length=50)
     direction = models.CharField(max_length=50)
 
