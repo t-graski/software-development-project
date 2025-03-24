1a4fa1928dcd33e75ba491f2a7b41c5c64f2e3a8
Tobias Graski <57156093+t-graski@users.noreply.github.com>
2025-03-24 14:47:49 +0000

    Add healthcheck template

--- backend/healthCheck/views.py
+++ backend/healthCheck/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from healthCheck.models import Employee
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the healthCheck index.")
+    employees = Employee.objects.all()
+    template = loader.get_template("healthChecks/index.html")
+    context = {
+        "employees": employees
+    }
+    return HttpResponse(template.render(context, request))
